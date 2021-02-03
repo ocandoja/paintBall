3dacd49fdb295fc5ba003d7fa3164b7fa1a07232
José Ocando <jaogocando@gmail.com>
2021-02-03 11:24:08 -0500

    Weapon
    weapon and movement player added

---
+++ paintBall/Assets/Scripts/weaponFire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponFire : MonoBehaviour
+{
+    public float damage = 1f;
+    public float range = 150;
+    public Camera pCamera;
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonDown("Fire1"))
+       {
+           Shoot();
+       } 
+    }
+    void Shoot ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(pCamera.transform.position, pCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
